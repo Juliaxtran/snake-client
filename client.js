005ad52db3bffdc5d999f5b37ca7f36410711911
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-16 14:07:40 -0400

    Client Server

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+  conn.setEncoding("utf8");
+  // interpret incoming data as text
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: JT");
+  }
+  )
+  conn.on("data", (data) => {
+    return console.log("Server outputs:", data);
+  });
+  return conn
+};
+
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
