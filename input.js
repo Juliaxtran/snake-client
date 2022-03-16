34180fa6677d4256aecbda94c7acab532d5917d0
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-03-16 16:42:42 -0400

    Messages

--- input.js
+++ input.js
@@ -5,7 +5,7 @@ const setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
-  stdin.resume();``
+  stdin.resume();
   stdin.on('data', handleUserInput);
   return stdin;
 };
@@ -15,21 +15,30 @@ const setupInput = function (conn) {
   const handleUserInput = function (key) {
     if (key === '\u0003') {
       process.exit();
-    } else if (key === "w") {
+    }  
+    if (key === 'w') {
       connection.write('Move: up');``
-    } else if (key === "a") {
+    } 
+    if (key === 'a') {
       connection.write('Move: left');
-    } else  if (key === "s") {
+    } 
+    if (key === 's') {
       connection.write('Move: down');
-    } else if (key === "d") {
+    } 
+    if (key === 'd') {
       connection.write('Move: right');
-    }
+    }  
+    if (key === 'z') {
+      connection.write('Say: Hey!');
+    };
+    if (key === 'p') {
+      connection.write('Say: :)');
+    };
+    if (key === 't') {
+      connection.write('Say: Hungry');
+    };
   };
   
-
-
-
-
 
 
 module.exports = { setupInput };
